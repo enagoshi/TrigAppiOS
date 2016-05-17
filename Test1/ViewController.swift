1a8340ff7e72809a0b99d4c9339d1f0bc840fc15
iPad App Development <ipad@cart115-lap02.local>
2016-05-17 08:40:46 -1000

    Initial Commit

---
+++ Test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by iPad App Development on 5/17/16.
+//  Copyright © 2016 Iolani School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
